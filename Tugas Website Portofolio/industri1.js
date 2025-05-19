13c37c89815fbc78ff9308f00a6fedece815769f
felllll4 <devinsekolah48@gmail.com>
2025-05-19 13:23:39 +0700

    Delete Tugas Website Portofolio directory

--- Tugas Website Portofolio/industri1.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const learnMoreBtn = document.getElementById('learnMoreBtn');
-    const moreInfo = document.getElementById('moreInfo');
-
-    learnMoreBtn.addEventListener('click', () => {
-        moreInfo.classList.toggle('hidden');
-        learnMoreBtn.textContent = moreInfo.classList.contains('hidden') ? 'Pelajari Lebih Lanjut' : 'Sembunyikan Info';
-    });
-});
